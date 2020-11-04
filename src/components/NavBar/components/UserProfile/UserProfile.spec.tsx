0381e6574d8dd5e28cd7da2ffe9f40ab1da28998
senkuu <dorosario.enzo@protonmail.com>
2020-11-04 23:48:36 +0100

    improve UserProfile tests

--- src/components/NavBar/components/UserProfile/UserProfile.spec.tsx
+++ src/components/NavBar/components/UserProfile/UserProfile.spec.tsx
@@ -1,9 +1,21 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+
+import { render, screen, fireEvent } from "@testing-library/react";
 
 import UserProfile from "./UserProfile";
 
 it("should renders component with the valid username", () => {
-  render(<UserProfile username="John Doo" disconnectUser={jest.fn()} />);
-  expect(screen.getByText("John Doo")).toBeInTheDocument();
+  render(<UserProfile username="John Doe" disconnectUser={jest.fn()} />);
+  expect(screen.getByText("John Doe")).toBeInTheDocument();
+});
+
+it("should call disconnectUser function in props when logout button is pressed", () => {
+  const fakeDisconnectUser = jest.fn();
+  const { getByTestId } = render(
+    <UserProfile username="John Doe" disconnectUser={fakeDisconnectUser} />
+  );
+
+  fireEvent.click(getByTestId("userprofile-button"));
+  fireEvent.click(getByTestId("userprofile-logout-button"));
+  expect(fakeDisconnectUser).toBeCalled();
 });
