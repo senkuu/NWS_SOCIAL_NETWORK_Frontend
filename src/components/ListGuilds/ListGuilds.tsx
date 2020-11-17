983de8b0f20d24265dc44f33797f7b1e0c6f77f1
RemiSchneider76 <remischneider76@gmail.com>
2020-11-17 12:32:39 +0100

    Ajout de composants

--- src/components/ListGuilds/ListGuilds.tsx
+++ src/components/ListGuilds/ListGuilds.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Typography from "@material-ui/core/Typography";
 
 import fetchGuilds from "services/guilds/fetchGuilds";
@@ -11,7 +11,12 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
 import ImageIcon from '@material-ui/icons/Image';
 import {Link, LinkProps} from 'react-router-dom'
+import Button from "@material-ui/core/Button";
+import {joinGuild} from "services/guilds/guilds"
 
+
+// import contexts
+import { UserContext } from "services/contexts/UserContext";
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
   
@@ -23,18 +28,24 @@ const useStyles = makeStyles((theme: Theme) =>
       maxWidth: 360,
       backgroundColor: theme.palette.background.paper,
     },
+    margin: {
+      margin: theme.spacing(1),
+    },
    })
 );
 
 function ListGuilds() {
     const [guildsList, setGuildsList] = useState<Guild[]>([]);
     const classes = useStyles();
+    const { user } = useContext(UserContext);
     useEffect(() => {
       fetchGuilds()
         .then((guilds) => setGuildsList(guilds))
         .catch((e) => console.error(e));
     }, []);
     
+
+  if (user) {
     return (
       <React.Fragment>
         <Typography variant="h3" className={classes.title} >Liste des guildes</Typography>
@@ -49,7 +60,10 @@ function ListGuilds() {
                 </Avatar>
               </ListItemAvatar>
               <ListItemText primary={guild.name} secondary={guild.description} />
-              <Link to={`/guild/${guild.id}`}>test</Link>
+              <Button onClick={() => joinGuild(guild.id.toString(), user.id.toString())} variant="contained" size="small" color="primary" className={classes.margin}>
+              Rejoindre
+              </Button>
+              <Link to={`/guild/${guild.id}`}>voir guilde</Link>
             </ListItem>
           </List>
         </Typography>)
@@ -58,6 +72,31 @@ function ListGuilds() {
           )}
       </React.Fragment>
     );
+  }
+
+  return(
+    <React.Fragment>
+        <Typography variant="h3" className={classes.title} >Liste des guildes</Typography>
+        
+        {guildsList.length > 0 ? (
+          guildsList.map((guild) => <Typography key={guild.id}>
+          <List className={classes.root}>
+            <ListItem>
+              <ListItemAvatar>
+                <Avatar>
+                  <ImageIcon />
+                </Avatar>
+              </ListItemAvatar>
+              <ListItemText primary={guild.name} secondary={guild.description} />
+              <Link to={`/guild/${guild.id}`}>voir guilde</Link>
+            </ListItem>
+          </List>
+        </Typography>)
+          ) : (
+            <Typography>Chargement</Typography>
+          )}
+      </React.Fragment>
+  );
 }
 
 export default ListGuilds;
